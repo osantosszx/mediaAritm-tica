06b9cfde8a3d2d1f41f4c6a6321ac8a8a9174ebf
guilherme santos <guiss250507@gmail.com>
2025-10-03 22:28:04 -0300

    finalizar atividade de media aritmetica

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿double NR1, n2, n3, media;
+Console.WriteLine("Média Aritmética de 3 números");
+
+Console.Write("Digite o primeiro número: ");
+NR1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Digite o segundo número: ");
+n2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Digite o terceiro número: ");
+n3 = Convert.ToDouble(Console.ReadLine());
+media = (NR1 + n2 + n3) / 3;
+Console.Write("media: ");
+Console.Write($"{media:N1}");
